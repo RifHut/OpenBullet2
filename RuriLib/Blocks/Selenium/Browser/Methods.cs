a45ae46bcfcf80200cfc820d5b8e185e5a3d43cf
Antec <Antec@DESKTOP-2COVGOS>
2024-03-28 19:48:47 +0100

    Define the path to Chrome extensions

--- RuriLib/Blocks/Selenium/Browser/Methods.cs
+++ RuriLib/Blocks/Selenium/Browser/Methods.cs
@@ -15,6 +15,7 @@ using RuriLib.Helpers;
 using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis.Differencing;
 using System.IO;
+using System.Security.Policy;
 
 namespace RuriLib.Blocks.Selenium.Browser
 {
@@ -70,27 +71,13 @@ namespace RuriLib.Blocks.Selenium.Browser
                     {
                         chromeop.AddArgument("--headless");
                     }
-
                     // Define the path to the directory that contains the Chrome extensions
-                    string extensionsDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "UserData", "ChromeExtensions");
-
-                    // Check if the directory exists
-                    if (Directory.Exists(extensionsDir))
+                    else if (data.ConfigSettings.BrowserSettings.ChromeExtensions.Count >0)
                     {
-                        // Get all the .crx files in the directory
-                        string[] extensionFiles = Directory.GetFiles(extensionsDir, "*.crx");
+                        chromeop.AddExtensions(data.ConfigSettings.BrowserSettings.ChromeExtensions
+                                    .Where(ext => ext.EndsWith(".crx"))
+                                    .Select(ext => Path.Combine(Directory.GetCurrentDirectory(), "UserData", "ChromeExtensions", ext)));
 
-                        // Add each extension to the options
-                        foreach (string extensionPath in extensionFiles)
-                        {
-                            chromeop.AddExtension(extensionPath);
-                        }
-                    }
-                    else
-                    {
-
-                        Console.WriteLine("UserData/ChromeExtensions directory does not exist.");
-                        return;
                     }
 
                     // Initialize the Chrome driver with the options
