0355ee34a754ac6e753c2ed9877d257248499570
Antec <Antec@DESKTOP-2COVGOS>
2024-03-29 08:22:20 +0100

    RollBack Exit ChromeExtensionFile Settings

--- OpenBullet2/Startup.cs
+++ OpenBullet2/Startup.cs
@@ -81,6 +81,7 @@ namespace OpenBullet2
             services.AddScoped<IWordlistRepository>(service => 
                 new HybridWordlistRepository(service.GetService<ApplicationDbContext>(),
                 "UserData/Wordlists"));
+
             // Singletons
             services.AddSingleton<UpdateService>();
             services.AddSingleton<AnnouncementService>();
--- RuriLib/Services/RuriLibSettingsService.cs
+++ RuriLib/Services/RuriLibSettingsService.cs
@@ -15,16 +15,12 @@ namespace RuriLib.Services
         private string EnvFile => Path.Combine(BaseFolder, "Environment.ini");
         private string RlSettFile => Path.Combine(BaseFolder, "RuriLibSettings.json");
 
-        private string ChromeExtensionFile => Path.Combine(BaseFolder, "google.crx");
-
         public EnvironmentSettings Environment { get; set; }
         public GlobalSettings RuriLibSettings { get; set; }
 
         public RuriLibSettingsService(string baseFolder)
         {
             BaseFolder = baseFolder;
-            
-
             Directory.CreateDirectory(baseFolder);
 
             jsonSettings = new JsonSerializerSettings 
@@ -43,33 +39,7 @@ namespace RuriLib.Services
             RuriLibSettings = File.Exists(RlSettFile)
                 ? JsonConvert.DeserializeObject<GlobalSettings>(File.ReadAllText(RlSettFile), jsonSettings)
                 : CreateGlobalSettings();
-
-            AddChromeExtensionIfExist();
         }
-        public Task AddChromeExtensionIfExist()
-        {
-            // Check if the google.crx file exists
-            if (File.Exists(ChromeExtensionFile))
-            {
-                // Path to the ChromeExtensions directory
-                var chromeExtensionsDir = Path.Combine(BaseFolder, "ChromeExtensions");
-
-                // Ensure the ChromeExtensions directory exists
-                Directory.CreateDirectory(chromeExtensionsDir);
-
-                // Path to the destination file
-                var destinationFile = Path.Combine(chromeExtensionsDir, "google.crx");
-
-                // Copy the google.crx file to the ChromeExtensions directory
-                if (!File.Exists(destinationFile))
-                {
-                    File.Copy(ChromeExtensionFile, destinationFile);
-                }
-            }
-            // Since there's no async work done here anymore, we can return a completed task
-            return Task.CompletedTask;
-        }
-
 
         /// <summary>
         /// Saves the settings to the designated file.
